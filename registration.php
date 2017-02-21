d0b7c7cc1e47cc0635c9bde8bc94853f97f3a540
Raul E Watson <diazwatson@gmail.com>
2017-02-21 10:31:55 +0000

    Remove 2 from module name

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Space48_PriceSlider2',
+    'Space48_PriceSlider',
     __DIR__
 );
