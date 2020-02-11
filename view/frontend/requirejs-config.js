9bc2eda779e04dc99082421caf21aa6b93ebd311
Nick <nick@space48.com>
2020-02-11 15:49:34 +0000

    Namespace touch-punch to Price Slider extension (#6)
    Just specifying the requirement as jquery/ui/touch-punch appears to
    trigger some default Magento functionality that tries to load from
    /jquery/compat/touch-punch.js.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,6 +1,11 @@
 var config = {
     paths: {
         'priceSlider': 'Space48_PriceSlider/js/price-slider',
-        'jquery/ui/touch-punch': 'Space48_PriceSlider/js/lib/jquery.ui.touch-punch.min'
+        'priceSlider/touch-punch': 'Space48_PriceSlider/js/lib/jquery.ui.touch-punch.min'
+    },
+    shim: {
+        'priceSlider/touch-punch': {
+            deps: ['jquery/ui']
+        }
     }
 };
