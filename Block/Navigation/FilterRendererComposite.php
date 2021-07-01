4b6151598c57b6e2dfcc3d510c73b7239dd721b0
Stepan Kechedzhi <stepan.kechedzhi@space48.com>
2021-07-01 14:10:17 +0300

    MF-1017 fix: replaced DI injected blocks in FilterRendererComposite with child blocks from layout

--- Block/Navigation/FilterRendererComposite.php
+++ Block/Navigation/FilterRendererComposite.php
@@ -10,36 +10,18 @@ use Magento\LayeredNavigation\Block\Navigation\FilterRenderer;
 class FilterRendererComposite extends AbstractBlock implements FilterRendererInterface
 {
     /**
-     * @var \Magento\LayeredNavigation\Block\Navigation\FilterRenderer
-     */
-    private $defaultRenderer;
-
-    /**
-     * @var array
-     */
-    private $filterBlocks;
-
-    public function __construct(
-        FilterRenderer $filterRenderer,
-        $filterBlocks = []
-    ) {
-        $this->defaultRenderer = $filterRenderer;
-        $this->filterBlocks = $filterBlocks;
-    }
-
-    /**
      * @param FilterInterface $filter
      * @return string
      */
     public function render(FilterInterface $filter)
     {
-        if (isset($this->filterBlocks[$filter->getRequestVar()])) {
-            /** @var FilterRendererInterface $block */
-            $block = $this->filterBlocks[$filter->getRequestVar()];
-            $html = $block->render($filter);
-        } else {
-            $html = $this->defaultRenderer->render($filter);
+        /** @var FilterRendererInterface $block */
+        $block = $this->getChildBlock('renderer_' . $filter->getRequestVar());
+        if (!$block) {
+            $block = $this->getChildBlock('renderer_default');
         }
+
+        $html = $block ? $block->render($filter) : '';
 
         return $html;
     }
